5551aefc84797240e4a8f07bf1bbe249d0f67dc4
Reinaldo A. C. Rauch <reinaldorauch@gmail.com>
2023-06-22 10:57:33 -0300

    using safeLoad for tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,5 +3,5 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = \Dotenv\Dotenv::createMutable(__DIR__);
-$dotenv->load();
+$dotenv->safeLoad();
 $dotenv->required(['DATABASE_URL', 'SECRET']);
